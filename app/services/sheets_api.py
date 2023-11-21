3fe42e50fd8bf0189cf77456787e59a76be45bfc
Anastasia Sokolova <navukso@ya.ru>
2023-11-22 00:24:42 +0300

    1

--- app/services/sheets_api.py
+++ app/services/sheets_api.py
@@ -90,9 +90,7 @@ async def get_spreadsheets_from_disk(
     service = await wrapper_service.discover(
         'drive', settings.google_drive_api_version)
     spreadsheets = await wrapper_service.as_service_account(
-        service.files.list(
-            q=f'mimeType="application/vnd.google-apps.spreadsheet"'
-              f'and name="{spreadsheet_title}"')
+        service.files.list(q=f'mimeType="application/vnd.google-apps.spreadsheet" and name="{spreadsheet_title}"')
     return spreadsheets['files']
 
 
